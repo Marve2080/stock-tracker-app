d3f137f088368a3e79ca4cd579fb0d21a57273db
Expert <marvelousr39@gmail.com>
2025-10-05 10:37:48 -0700

    Implement Auth & Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 const Layout = ({children} :{children : React.ReactNode}) => {
     return (
-            <main className="min-h-screen text-grey-400">
+            <main className="min-h-screen text-gray-400">
                 <Header />
                 <div className = " container py-10">
                     {children}
