9e2decb832d95a5250c2242b19c862e0e9e88373
Expert <marvelousr39@gmail.com>
2025-10-07 11:24:26 -0700

    implement auth logic

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -12,15 +12,16 @@ import {Button} from "@/components/ui/button"
 import {useRouter} from "next/navigation";
 import {LogOut} from "lucide-react";
 import NavItems from "@/components/NavItems";
+import {signOut} from "@/lib/actions/auth.actions";
 
-const UserDropdown = () => {
+const UserDropdown = ({ user }: {user: User}) => {
     const router= useRouter();
 
     const handleSignOut = async () => {
+        await signOut();
         router.push("/sign-in");
     }
 
-    const user = {name: 'john', email: 'contact@marvel.com'};
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
